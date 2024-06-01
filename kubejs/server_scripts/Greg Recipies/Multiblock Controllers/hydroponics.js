de352b8fef606067c88c5178551d3ae2de5434df
Cattman423 <59346928+Cattman423@users.noreply.github.com>
2024-05-31 20:28:10 -0700

    Hydroponic Farm

--- kubejs/server_scripts/Greg Recipies/Multiblock Controllers/hydroponics.js
+++ kubejs/server_scripts/Greg Recipies/Multiblock Controllers/hydroponics.js
@@ -1,36 +1,102 @@
 ServerEvents.recipes(event => {
-    event.recipes.gtceu.assembler('advanced_alternator')
+    event.recipes.gtceu.assembler('hydroponic_farm')
         .circuit(5)
         .itemInputs(
             'gtceu:ev_machine_hull', 
             '4x #gtceu:circuits/ev', 
             '4x gtceu:stainless_steel_frame', 
             '4x gtceu:ev_electric_motor', 
-            '16x gtceu:small_stainless_steel_pipe',
+            '16x gtceu:stainless_steel_small_fluid_pipe',
             '2x gtceu:fluid_filter'
         )
         .itemOutputs('gtceu:hydroponic_farm')
         .duration(min*5)
         .EUt(ev)
 
-    function farming (id, input, output){
+    function farming (id, cid, input, fluid, output){
         event.recipes.gtceu.hydroponic_farm(id)
-            .itemInputs(input)
-            .inputFluids('minecraft:water 1000')
+            .circuit(cid)
+            .notConsumable(input)
+            .inputFluids(fluid)
             .itemOutputs(output)
             .duration(min)
             .EUt(iv)
     }
 
-    const mccrops = ['wheat', 'carrots', 'potatoes', 'beatroot', 'pumpkin', 'melon', 'coca_beans']
-    const pamscrops = []
+    const mccrops = ['carrot', 'potato', 'cocoa_beans']
+    const mcseed = ['wheat', 'beetroot', 'pumpkin', 'melon', 'torchflower']
+    const pamscrops = ['agave', 'amaranth', 'arrowroot', 'artichoke', 'asparagus', 'barley', 'bean', 'bellpepper', 'blackberry', 'blueberry', 'broccoli', 'brusselsprout', 'cabbage', 
+    'cactusfruit', 'candleberry', 'cantaloupe', 'cassava', 'cauliflower', 'celery', 'chickpea', 'chilipepper', 'coffeebean', 'corn', 'cotton', 'cranberry', 'cucumber', 'eggplant', 
+    'elderberry', 'flax', 'garlic', 'ginger', 'grape', 'greengrape', 'huckleberry', 'jicama', 'juniperberry', 'jute', 'kale', 'kenaf', 'kiwi', 'kohlrabi', 'leek', 'lentil', 'lettuce', 
+    'millet', 'mulberry', 'mustardseeds', 'oats', 'okra', 'onion', 'parsnip', 'peanut', 'peas', 'pineapple', 'quinoa', 'radish', 'raspberry', 'rhubarb', 'rice', 'rutabaga', 'rye', 
+    'scallion', 'sesameseeds', 'sisal', 'soybean', 'spiceleaf', 'spinach', 'strawberry', 'sweetpotato', 'taro', 'tealeaf', 'tomatillo', 'tomato', 'turnip', 'waterchestnut', 
+    'whitemushroom', 'wintersquash', 'zucchini', 'alfalfa', 'aloe', 'barrelcactus', 'canola', 'cattail', 'chia', 'cloudberry', 'lotus', 'nettles', 'nopales', 'sorghum', 'truffle', 
+    'wolfberry', 'yucca', 'bokchoy', 'calabash', 'guarana', 'papyrus', 'sunchoke']
     const fdcrops = []
 
-    mccrops.forEach( (base) => {
-        let id = 'kubejs:gtceu/hydroponic_farm/' + base
-        let input = 'minecraft:' + base + '_seeds'
-        let output = 'minecraft:' + base
+    for (let i = 1; i < 9; i++) {
+        mccrops.forEach( (base) => {
+            let id = 'kubejs:gtceu/hydroponic_farm/mc_' + base + '_' + i.toString()
+            let input = 64*i.toString() + 'x ' + 'minecraft:' + base
+            let output = 128*i.toString() + 'x ' + 'minecraft:' + base
+            let water = 'minecraft:water ' + 1000*i.toString()
+            let cid = i
 
-        farming(id, input, output)
-    })
+            farming(id, cid, input, water, output)
+        })
+        mcseed.forEach( (base) => {
+            let id = 'kubejs:gtceu/hydroponic_farm/mc_' + base + '_' + i.toString()
+            let input = 64*i.toString() + 'x ' + 'minecraft:' + base + '_seeds'
+            let output = 64*i.toString() + 'x ' + 'minecraft:' + base
+            let water = 'minecraft:water ' + 1000*i.toString()
+            let cid = i
+
+            farming(id, cid, input, water, output)
+        })
+        pamscrops.forEach( (base) => {
+            let id = 'kubejs:gtceu/hydroponic_farm/pamhc2_' + base + '_' + i.toString()
+            let input = 64*i.toString() + 'x ' + 'pamhc2crops:' + base + 'seeditem'
+            let output = 64*i.toString() + 'x ' + 'pamhc2crops:' + base + 'item'
+            let water = 'minecraft:water ' + 1000*i.toString()
+            let cid = i
+
+            farming(id, cid, input, water, output)
+        })
+        /*farming(
+            'kubejs:gtceu/hydroponic_farm/fd_brown_mushroom_colony_' + i.toString(), 
+            i, 
+            64*i.toString() + 'farmersdelight:brown_mushroom_colony', 
+            128*i.toString() + 'farmersdelight:brown_mushroom_colony'
+        )
+        farming(
+            'kubejs:gtceu/hydroponic_farm/fd_red_mushroom_colony_' + i.toString(), 
+            i, 
+            64*i.toString() + 'farmersdelight:red_mushroom_colony', 
+            128*i.toString() + 'farmersdelight:red_mushroom_colony'
+        )
+        farming(
+            'kubejs:gtceu/hydroponic_farm/fd_cabbage_' + i.toString(), 
+            i, 
+            64*i.toString() + 'farmersdelight:cabbage_seeds', 
+            128*i.toString() + 'farmersdelight:cabbage'
+        )
+        farming(
+            'kubejs:gtceu/hydroponic_farm/fd_tomato_' + i.toString(), 
+            i, 
+            64*i.toString() + 'farmersdelight:tomato_seeds', 
+            128*i.toString() + 'farmersdelight:tomato'
+        )
+        farming(
+            'kubejs:gtceu/hydroponic_farm/fd_onion_' + i.toString(), 
+            i, 
+            64*i.toString() + 'farmersdelight:onion', 
+            128*i.toString() + 'farmersdelight:onion'
+        )
+        farming(
+            'kubejs:gtceu/hydroponic_farm/fd_rice_' + i.toString(), 
+            i, 
+            64*i.toString() + 'farmersdelight:rice', 
+            128*i.toString() + 'farmersdelight:rice_panicle'
+        )*/
+    }
 })
--- kubejs/startup_scripts/gtceu/Multiblocks/hydroponic_farm.js
+++ kubejs/startup_scripts/gtceu/Multiblocks/hydroponic_farm.js
@@ -1,34 +1,38 @@
 GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
-//Builder's press
+//Hydroponic Farm
     event.create('hydroponic_farm')
         .category('multiblock')
         .setEUIO('in')
-            .setMaxIOSize(3, 6, 3, 3) //Max Item Inputs, Max Item Outputs, Max Fluid Inputs, Max Fluid Outputs
+            .setMaxIOSize(9, 16, 2, 0) //Max Item Inputs, Max Item Outputs, Max Fluid Inputs, Max Fluid Outputs
         //.setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.ARC)
 })
 GTCEuStartupEvents.registry('gtceu:machine', event => {
-//Builder's press
+//Hydroponic Farm
     event.create('hydroponic_farm', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
         .recipeType('hydroponic_farm')
         .appearanceBlock(GTBlocks.CASING_STEEL_SOLID)
         .pattern(definition => FactoryBlockPattern.start()
-            .aisle("CCCCCCCC", "CCCCCCCC", "CGGGGGGC", "CCCCCCCC")
-            .aisle("CCCCCCCC", "CDDDDDDC", "G      I", "CGGGGGGC").setRepeatable(1, 16)
-            .asile("CCCCCCCC", "CCCACCCC", "CGGGGGGC", "CCCCCCCC")
+            .aisle('CCCCCCCCCCC', 'CCCCCCCCCCC', 'CGGGGGGGGCC', 'CCCCCCCCCCC')
+            .aisle('EEEEEEEEEEE', 'EDDDDDDDDEE', 'G        PI', 'EGGGGGGGGEE').setRepeatable(1, 8)
+            .aisle('CCCCCCCCCCC', 'CCCCCACCCCC', 'CGGGGGGGGCC', 'CCCCCCCCCCC')
             .where('A', Predicates.controller(Predicates.blocks(definition.get())))
             .where('I', Predicates.blocks('gtceu:ulv_input_bus'))
-            .where('G', Predicates.blocks(GTBlocks.CASING_LAMINATED_GLASS.get()))
-            .where("C", Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get())
+            .where('D', Predicates.blocks('minecraft:dirt'))
+            .where('P', Predicates.blocks(GTBlocks.CASING_STEEL_PIPE.get()))
+            .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
+            .where('E', Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get()))
+            .where('C', Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get())
                     .or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1))
-                    .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMaxGlobalLimited(2))
-                    .or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMaxGlobalLimited(2))
-                    .or(Predicates.abilities(PartAbility.EXPORT_ITEMS)))
+                    .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMinGlobalLimited(1).setMaxGlobalLimited(2))
+                    .or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMinGlobalLimited(1).setMaxGlobalLimited(2))
+                    .or(Predicates.abilities(PartAbility.EXPORT_ITEMS).setMinGlobalLimited(1)))
+            .where(' ', Predicates.any())
             .build()
         )
         .workableCasingRenderer(
-            "gtceu:block/casings/solid/machine_casing_solid_steel",
-            "gtceu:block/multiblock/implosion_compressor", false
+            'gtceu:block/casings/solid/machine_casing_solid_steel',
+            'gtceu:block/multiblock/implosion_compressor', false
         )
 })
